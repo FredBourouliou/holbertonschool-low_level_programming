8c6d4dfd79134de44bf75b24a98fd0196db7b7b9
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-21 14:58:50 +0200

    Update file 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,9 +8,7 @@
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful"\" - Dora Korpar, 2015-10-19"\");
-	while (*str) {;
-       	putchar(*str++);
+	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19"\");
 	}
 	return (1);
 }
