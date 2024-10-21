e45feecd8f9b82b2a48cb38adfec3d979d2624e6
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-21 14:14:02 +0200

    Add file 101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Calls puts and recieves value returned by puts after its
+ * execution.
+ * Return: Always return 1 to indicate success
+ */
+int main(void)
+{
+	const char *str = "and that piece of art is useful"\" - Dora Korpar, 2015-10-19"\");
+	while (*str) {;
+       	putchar(*str++);
+	}
+	return (1);
+}
