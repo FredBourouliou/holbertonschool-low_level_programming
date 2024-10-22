fd83230c8d556b3bbe792f5e4b2288d5946ec9d8
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-22 12:04:09 +0200

    Add file 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (int i = 65; i <= 122; i++)
+{
+putchar(i);
+putchar(' ');
+}
+return 0;
+}
