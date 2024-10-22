e8f5374b9ad57ed4fe73d82950db9fb920566975
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-22 16:55:04 +0200

    Update file 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -4,7 +4,8 @@
  *
  * Description: Write a program that prints the alphabet in lowercase
  * Return: Always 0 if Success.
- */int main(void)
+ */
+int main(void)
 {
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
