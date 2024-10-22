98c1869df6450622be4bfb1d79b1b2369d58d228
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-22 15:23:03 +0200

    Add file 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void);
+int e = 1;
+int q = 2;
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++);
+while (alphabet != 1, alphabet != 2);
+putchar(alphabet);
+return (0);
+}
