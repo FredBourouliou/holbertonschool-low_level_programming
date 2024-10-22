bf0080997a25ea51b341afdffe0bdc887c72e8d8
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-22 14:19:47 +0200

    Update file 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-
 /**
 * main - Entry point
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-for (ch= ‘A’; ch<= ‘Z’; ch++)
-putchar(ch);
-putchar (‘\n’ ) ;
-for (ch= ‘a’; ch<= ‘z’; ch++)
-putchar(ch);
-putchar (‘\n’) ;
-getch();
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
+putchar(alphabet);
+putchar('\n');
 return (0);
 }
