547dbdd60fb66640be3238d43632de62470a501d
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-22 12:07:21 +0200

    Update file 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-#include <math.h>
+
 /**
 * main - Entry point
 * Return: Always 0 (Success)
@@ -13,5 +12,5 @@ for (int i = 65; i <= 122; i++)
 putchar(i);
 putchar(' ');
 }
-return 0;
+return (0);
 }
