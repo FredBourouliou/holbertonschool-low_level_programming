b638034b7acc95af4670dd6ed4545659ea293dbf
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-22 20:57:17 +0200

    Update file 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,11 +3,11 @@
 * main - Entry point
 * Return: Always 0 (Success)
 */
-int main(void)
 {
-for (int i = 0; i <= 9; i++)
+int i;
+for (i = 0; i <= 9; i++)
 {
 printf("%d\n", i);
 }
-return (0);
+return 0;
 }
