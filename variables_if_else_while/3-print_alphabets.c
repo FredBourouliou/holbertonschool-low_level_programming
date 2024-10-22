55f92424884cf4007af16cfca6f6f6c415045685
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-22 14:45:28 +0200

    Update file 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,8 @@ int main(void)
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 putchar(alphabet);
-putchar(toupper(alphabet));
+for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
+putchar(alphabet);
 putchar('\n');
 return (0);
 }
