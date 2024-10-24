3cdb0b77b9ee4045a3718cdb80968fde9f8f826e
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-24 22:23:38 +0200

    Update file 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Return: Always 0 success
  *
  */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 char i;
 for (i = 'a'; i <= 'z'; i++)
