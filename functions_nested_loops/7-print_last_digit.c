7e207249a769eece2641a00e390f8df9c63cf9af
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-25 14:38:33 +0200

    Update file 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _abs - Computes the absolute value of an integer.
+  * @c: The number to be computed.
+  *
+  * Return: Absolute value of number or zero
+  */
+
+int print_last_digit(int)
+{
+int r;
+print_last_digit(98);
+print_last_digit(0);
+r = print_last_digit(-1024);
+_putchar('0' + r);
+_putchar('\n');
+return (0);
+}
