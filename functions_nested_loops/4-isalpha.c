ea062354fbe83d5841e758e812abe52d9c5a9c48
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-25 10:24:19 +0200

    update file 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -5,16 +5,10 @@
  * Return: 1 for letter or 0 for anything else
  */
 int _isalpha(int c)
-{    
-int c;
-c = _isalpha('H');
-_putchar(c + '0');
-c = _isalpha('o');
-_putchar(c + '0');
-c = _isalpha(108);
-_putchar(c + '0');
-c = _isalpha(';');
-_putchar(c + '0');
-_putchar('\n');
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
 return (0);
 }
