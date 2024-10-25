48b0b693a242adefa4bfc7325cd91bce43761b2e
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-25 12:03:34 +0200

    Update file 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,10 +2,16 @@
 #include <stdio.h>
 /**
  * _abs - main entry
+ * @n: the number
  * return: always 0 success
  */
 int _abs(int n)
 {
-int abs (n);
-return (0);
+if (n < 0)
+{
+int abs_val;
+abs_val = n * -1;
+return (abs_val);
+}
+return (n);
 }
