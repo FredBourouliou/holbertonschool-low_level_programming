39f059bba629a44b4bfebbb428a5cffa0a22c6ca
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-25 12:07:56 +0200

    Update file 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _abs - Computes the absolute value of an integer
- * @n: the number
+ * _abs - Computes the absolute value of an integer.
+ * @n: the number.
  * return: absolute value of number zero
  */
 int _abs(int n)
