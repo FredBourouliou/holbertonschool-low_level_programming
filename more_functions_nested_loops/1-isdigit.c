e21dbe2cb815f017509f8e8427195f82e9e343f5
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-28 11:23:56 +0100

    Add file 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _isdigit(int c) - check the digit
+ *@c: the digit
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
