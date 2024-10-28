21db6ed6e82a3f59dc272e23ee4e72ca6ef764ff
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-28 15:15:25 +0100

    Add file 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * print_line - entry point
+  * @n: the number of times the character _ should be printed
+  * Return: Always 0 success
+  */
+void print_line(int n)
+{
+if (n > 0) 
+_putchar(_ * n);
+return (n);
