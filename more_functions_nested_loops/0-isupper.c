c6b48d22f668be826379aaf933375a93342ed4fb
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-28 10:59:25 +0100

    Add file 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (_isupper(c)) 
+{
+_putchar(1);
+} 
+else 
+{
+_putchar(0);
+}
+return (c);
+}
