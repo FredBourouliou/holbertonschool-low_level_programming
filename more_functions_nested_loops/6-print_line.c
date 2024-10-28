14a68b47c9bf2f24b24d9fe233e7ff9756a28fb3
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-28 15:49:58 +0100

    Update file 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,13 +2,21 @@
 /**
   * print_line - entry point
   * @n: the number of times the character _ should be printed
-  * Return: Always 0 success
+  * Return: empty 
   */
 void print_line(int n)
 {
-if (n > 0)
-_putchar('_' * n);
-else
-_putchar(\n);
+int x;
+if (n <= 0)
+{
 _putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
 }
