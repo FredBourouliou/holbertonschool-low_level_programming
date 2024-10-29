1cd5e89d1a85a1e696f3ad62734691dcfcc2d96a
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-29 10:16:16 +0100

    Add file 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: the size of the square
+ * return: always 0
+ */
+void print_square(int size)
+{
+int x;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
