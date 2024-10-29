8bc76ad6d71ac7a52aaacfe0d08050cce56ab974
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-29 09:53:24 +0100

    Update file 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -5,8 +5,8 @@
  * return: empty
  */
 void print_diagonal(int n)
-{
-int x;
+{ 
+int x, y;
 if (n <= 0)
 {
 _putchar('\n');
@@ -14,6 +14,13 @@ _putchar('\n');
 else
 {
 for (x = 0; x < n; x++)
-_putchar((92)'\n');
+{
+for (y = 0; y < x; y++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
 }
 }
