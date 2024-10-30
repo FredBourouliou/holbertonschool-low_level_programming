00afbb611c42020ea0c155b2580e3e5b01ab5103
Frederic Bourouliou <bourouliou@gmail.com>
2024-10-30 22:34:30 +0100

    Add file 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: integer
+ * @b: integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int a, b, temp;
+temp = a;
+a = b;
+b = temp;
+}
