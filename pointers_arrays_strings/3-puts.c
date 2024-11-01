461ae99a5e3b3ba2f136898309182534a2882012
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-01 07:13:39 +0100

    Add file 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * _puts - a function that prints a string, followed by a new line
+ * @str: The string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+while (*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
