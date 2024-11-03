b78591071bcab6155334b844d113c3b61a282873
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-03 10:29:45 +0100

    Update file 6 bis

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,26 @@
 #include "main.h"
 /**
- * *string_toupper - Converts all lowercase letters of a string to uppercase
- * @str: The string to be converted
- * Return: A pointer to the resulting string with all uppercase letters
+ * cap_string - Capitalizes all words of a string
+ * @str: The string to capitalize
+ * Return: A pointer to the resulting string with words capitalized
  */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 int i = 0;
- while (str[i] != '\0')
-{
+char separators[] = " \t\n,;.!?\"(){}";
 if (str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] = str[i] - ('a' - 'A');
+}
+while (str[i] != '\0')
+{
+for (int j = 0; separators[j] != '\0'; j++)
+{
+if (str[i] == separators[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] = str[i + 1] - ('a' - 'A');
+break;
+}
 }
 i++;
 }
