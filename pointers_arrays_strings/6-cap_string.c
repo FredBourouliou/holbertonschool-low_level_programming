250a3365f38fd699f37022a6c800854a74939533
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-03 10:37:38 +0100

    Update file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,6 +7,7 @@
 char *cap_string(char *str)
 {
 int i = 0;
+int j;
 char separators[] = " \t\n,;.!?\"(){}";
 if (str[i] >= 'a' && str[i] <= 'z')
 {
@@ -14,7 +15,7 @@ str[i] = str[i] - ('a' - 'A');
 }
 while (str[i] != '\0')
 {
-for (int j = 0; separators[j] != '\0'; j++)
+for (j = 0; separators[j] != '\0'; j++)
 {
 if (str[i] == separators[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
 {
