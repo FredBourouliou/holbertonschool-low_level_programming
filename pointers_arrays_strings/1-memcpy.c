e596bc730b6d189c86d4927564604b6052dacf4d
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-04 11:49:56 +0100

    Add file 1 tier

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * char *_memcpy(char *dest, char *src, unsigned int n) - function that copies memory area.
+ * @dest: memory area
+ * @src: constant byte to fill
+ * @n: bytes of memory area to fill
+ * Return: memory area filled
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+for (a = 0; a < n; a++)
+{
+dest[a] = *src;
+}
+return (dest);
+}
