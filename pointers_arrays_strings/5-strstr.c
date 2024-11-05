35db8325afbed8d704ad0fdd7791eae0f17e056f
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-05 11:11:59 +0100

    Add file 5 tier

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strstr - a function that locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int a = 0, b = 0;
+while (haystack[a])
+{
+while (needle[b])
+{
+if (haystack[a + b] != needle[b])
+{
+break;
+}
+b++;
+}
+if (needle[b] == '\0')
+{
+return (haystack + a);
+}
+a++;
+}
+return ('NULL');
+}
