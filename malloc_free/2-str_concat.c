dfcf908fc9c0c9f70f1cfbea0408ba8747594869
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-11 13:20:21 +0100

    Update files 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,9 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
- * *str_concat - Concatenates two strings into a newly allocated space
+ * str_concat - Concatenates two strings
  * @s1: The first string.
  * @s2: The second string.
  * Return: A pointer to the newly allocated space
@@ -12,10 +10,10 @@ char *str_concat(char *s1, char *s2)
 {
 char *concat_str;
 int len1 = 0, len2 = 0, i, j;
-if (s1 != NULL)
-len1 = strlen(s1);
-if (s2 != NULL)
-len2 = strlen(s2);
+while (s1 && s1[len1])
+len1++;
+while (s2 && s2[len2])
+len2++;
 concat_str = malloc((len1 + len2 + 1) * sizeof(char));
 if (concat_str == NULL)
 return (NULL);
