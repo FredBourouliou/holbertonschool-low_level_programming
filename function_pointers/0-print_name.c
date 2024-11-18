9fbbeb59f22a10299d1ab5cea55d15f45554faf7
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-18 13:28:28 +0100

    Update file 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -6,8 +6,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (f && name)
-        f(name);
-    if (f == NULL)
-        return;
+if (f && name)
+f(name);
+if (f == NULL)
+return;
 }
