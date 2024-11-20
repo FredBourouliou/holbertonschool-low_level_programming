d1fba8a8c9fbf9f12a9b40f819262e1bd0aa542e
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-20 16:22:26 +0100

    Update files for betty style

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-
 int sum_them_all(const unsigned int n, ...)
 {
 int sum = 0;
--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,28 +1,27 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
+/**
+ * print_strings - a function that prints strings 
+ * *separator: a character
+ * n: an integer
+ * Return: 0
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-
-    va_start(args, n);
-
-    for (i = 0; i < n; i++)
-    {
-        const char *str = va_arg(args, const char *);
-
-        if (str == NULL)
-            printf("(nil)");
-        else
-            printf("%s", str);
-
-        if (i < n - 1 && separator != NULL)
-            printf("%s", separator);
-    }
-
-    printf("\n");
-
-    va_end(args);
+va_list args;
+unsigned int i;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+const char *str = va_arg(args, const char *);
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s", str);
+if (i < n - 1 && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(args);
 }
