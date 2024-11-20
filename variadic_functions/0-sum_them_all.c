85c64c67bb6397e439e315d5c0862f822584bf1e
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-20 10:00:32 +0100

    Add files 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+int sum = 0;
+va_list args;
+unsigned int i;
+if (n == 0)
+return (0);
+va_start(args, n);
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
+va_end(args);
+return (sum);
+}
