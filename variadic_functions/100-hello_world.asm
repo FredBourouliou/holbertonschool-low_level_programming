94a018a0bb0653e8740bb7a1e5e0166a707217d2
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-22 18:40:28 +0100

    Update 100 hello world file

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,14 +1,16 @@
 section .data
-    msg db 'Hello, World', 0xA
-    len equ $ - msg
+msg db 'Hello, World', 0xA
+len equ $ - msg
+
 section .text
-    global _start
+global _start
+
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, len
-    syscall
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+mov rax, 1      ; write
+mov rdi, 1      ; stdout
+mov rsi, msg    ; address of message
+mov rdx, len    ; length of message
+syscall         ; perform system call
+mov rax, 60     ; exit
+xor rdi, rdi    ; status 0
+syscall         ; perform system call
