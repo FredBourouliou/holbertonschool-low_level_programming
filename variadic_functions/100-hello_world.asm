2f6223e820d4c0d0d372096f5001dec4ce5740a2
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-23 10:37:51 +0100

    Update file 100

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,13 +1,13 @@
-    .section __TEXT,__text
+    .section __TEXT,__text           // Section texte pour le code exécutable
     .global _start                   // Point d'entrée global
 
 _start:
     // Appel système write
-    mov x8, #64                      // Numéro du syscall pour write
+    mov x8, #64                      // Numéro du syscall pour write (64 pour ARM64 sur macOS)
     mov x0, #1                       // File descriptor: STDOUT
     adrp x1, msg                     // Charger la page de msg dans x1
-    add x1, x1, :lo12:msg            // Ajouter l'offset pour l'adresse complète
-    mov x2, #13                      // Longueur du message ("Hello, World\n")
+    add x1, x1, :lo12:msg            // Ajouter l'offset pour obtenir l'adresse complète
+    mov x2, #13                      // Longueur fixe du message "Hello, World\n"
     svc #0                           // Appel système
 
     // Appel système exit
@@ -15,6 +15,6 @@ _start:
     mov x0, #0                       // Code de retour : 0 (succès)
     svc #0                           // Appel système
 
-    .section __DATA,__data
+    .section __DATA,__data           // Section data pour les données
 msg:
-    .asciz "Hello, World\n"          // Message avec un saut de ligne
+    .asciz "Hello, World\n"          // Message ASCII avec un saut de ligne
