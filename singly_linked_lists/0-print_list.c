bdfbae2b997df523ed413075edf4721e4cc994b9
Frederic Bourouliou <bourouliou@gmail.com>
2024-12-02 14:37:45 +0100

    Update 0 print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "lists.h"
-
 /**
  * print_list - prints all elements of a list_t list
  * @h: pointer to the list to print
@@ -8,34 +8,12 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
-char *str;
-char num_str[20];
-int i, len;
 while (h != NULL)
 {
-_putchar('[');
 if (h->str == NULL)
-{
-_putchar('0');
-str = "(nil)";
-}
+printf("[0] (nil)\n");
 else
-{
-len = 0;
-i = h->len;
-do {
-num_str[len++] = i % 10 + '0';
-i /= 10;
-} while (i > 0);
-for (i = len - 1; i >= 0; i--)
-_putchar(num_str[i]);
-str = h->str;
-}
-_putchar(']');
-_putchar(' ');
-for (i = 0; str[i] != '\0'; i++)
-_putchar(str[i]);
-_putchar('\n');
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 count++;
 }
