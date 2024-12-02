89b9ef4107edbcb1578d5e3357679a71fb914734
Frederic Bourouliou <bourouliou@gmail.com>
2024-12-02 16:45:12 +0100

    Update files 4 betty style

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,22 +1,16 @@
 #include "lists.h"
-
 /**
  * free_list - frees a list_t list
  * @head: pointer to the head of the list
  */
 void free_list(list_t *head)
 {
-    list_t *current;
-
-    while (head != NULL)
-    {
-        /* Sauvegarder le prochain nœud */
-        current = head->next;
-        /* Libérer la chaîne allouée */
-        free(head->str);
-        /* Libérer le nœud */
-        free(head);
-        /* Passer au nœud suivant */
-        head = current;
-    }
+list_t *current;
+while (head != NULL)
+{
+current = head->next;
+free(head->str);
+free(head);
+head = current;
+}
 }
