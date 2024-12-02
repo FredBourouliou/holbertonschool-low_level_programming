6ed342195317de3318f9f9869282ac9119cbab0f
Frederic Bourouliou <bourouliou@gmail.com>
2024-12-02 18:14:09 +0100

    Update 2addnode et 3addnode

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,25 +1,41 @@
 #include <string.h>
 #include "lists.h"
+
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to pointer to the head of the list
  * @str: string to be duplicated and added to the new node
+ *
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-}
-new_node->len = strlen(str);
-new_node->next = *head;
-*head = new_node;
-return (new_node);
+	list_t *new_node;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	/* Allouer de la mémoire pour le nouveau nœud */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Dupliquer la chaîne avec strdup */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	/* Calculer la longueur de la chaîne */
+	new_node->len = strlen(str);
+
+	/* Faire pointer le nouveau nœud vers l'ancien premier nœud */
+	new_node->next = *head;
+
+	/* Faire pointer head vers le nouveau nœud */
+	*head = new_node;
+
+	return (new_node);
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,33 +1,51 @@
 #include <string.h>
 #include "lists.h"
+
 /**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to pointer to the head of the list
  * @str: string to be duplicated and added to the new node
+ *
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node, *temp;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-}
-new_node->len = strlen(str);
-new_node->next = NULL;
-if (*head == NULL)
-{
-*head = new_node;
-return (new_node);
-}
-temp = *head;
-while (temp->next != NULL)
-temp = temp->next;
-temp->next = new_node;
-return (new_node);
+	list_t *new_node, *temp;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	/* Allouer de la mémoire pour le nouveau nœud */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Dupliquer la chaîne avec strdup */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	/* Initialiser les autres champs du nouveau nœud */
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	/* Si la liste est vide, le nouveau nœud devient la tête */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* Parcourir jusqu'au dernier nœud */
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	/* Lier le dernier nœud au nouveau nœud */
+	temp->next = new_node;
+
+	return (new_node);
 }
