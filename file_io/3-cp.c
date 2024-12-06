25722851416740cb20b715939165fa5b65799c4c
Frederic Bourouliou <bourouliou@gmail.com>
2024-12-06 21:32:40 +0100

    Update file 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -43,21 +43,37 @@ int main(int argc, char *argv[])
 
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		error_handler(99, "Error: Can't write to %s\n", file_to);
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_handler(99, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		error_handler(99, "Error: Can't write to %s\n", file_to);
 	}
 
-	if (bytes_read == -1)
-		error_handler(98, "Error: Can't read from file %s\n", file_from);
+	while (1)
+	{
+		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_handler(98, "Error: Can't read from file %s\n", file_from);
+		}
+		if (bytes_read == 0)
+			break;
+
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_handler(99, "Error: Can't write to %s\n", file_to);
+		}
+	}
 
 	if (close(fd_from) == -1)
+	{
+		close(fd_to);
 		error_handler(100, "Error: Can't close fd %d\n", file_from);
-
+	}
 	if (close(fd_to) == -1)
 		error_handler(100, "Error: Can't close fd %d\n", file_to);
 
