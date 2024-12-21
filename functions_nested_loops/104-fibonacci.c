25fdd7ebf61fe20af171d02958349d6ad4e42656
Frederic Bourouliou <bourouliou@gmail.com>
2024-12-21 20:06:11 +0100

    Update 104-fibonacci

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,39 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints first 98 Fibonacci numbers, starting with 1 and 2
- * Return: Always 0 (Success)
+ * print_fibonacci_part2 - prints the rest of Fibonacci numbers
+ * @n1: first number
+ * @n2: second number
+ * @count: current count
  */
-int main(void)
+void print_fibonacci_part2(unsigned long int n1,
+		unsigned long int n2, int count)
 {
-	unsigned long int n1 = 1, n2 = 2;
 	unsigned long int n1_head, n1_tail, n2_head, n2_tail;
 	unsigned long int head, tail, div = 1000000000;
-	int count;
 
-	printf("%lu, %lu", n1, n2);
-
-	for (count = 3; count < 93; count++)
-	{
-		n1_tail = n1 % div;
-		n1_head = n1 / div;
-		n2_tail = n2 % div;
-		n2_head = n2 / div;
-
-		head = n1_head + n2_head;
-		tail = n1_tail + n2_tail;
-		if (tail >= div)
-		{
-			head += 1;
-			tail -= div;
-		}
-
-		printf(", %lu%09lu", head, tail);
-		n1 = n2;
-		n2 = (head * div) + tail;
-	}
-
-	/* After 92 numbers, we need to handle numbers separately */
 	n1_tail = n1 % div;
 	n1_head = n1 / div;
 	n2_tail = n2 % div;
@@ -49,7 +27,10 @@ int main(void)
 			tail -= div;
 		}
 
-		printf(", %lu%09lu", head, tail);
+		if (head > 0)
+			printf(", %lu%09lu", head, tail);
+		else
+			printf(", %lu", tail);
 
 		n1_head = n2_head;
 		n1_tail = n2_tail;
@@ -57,5 +38,27 @@ int main(void)
 		n2_tail = tail;
 	}
 	printf("\n");
+}
+
+/**
+ * main - prints first 98 Fibonacci numbers, starting with 1 and 2
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int n1 = 1, n2 = 2, next;
+	int count;
+
+	printf("%lu, %lu", n1, n2);
+
+	for (count = 3; count < 93; count++)
+	{
+		next = n1 + n2;
+		printf(", %lu", next);
+		n1 = n2;
+		n2 = next;
+	}
+
+	print_fibonacci_part2(n1, n2, count);
 	return (0);
 }
